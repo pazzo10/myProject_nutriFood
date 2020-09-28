4c97d1e5bffb1e82d74ee4ad58741802f51ceb7d
pazzo10 <patrickrene.mwimanzi@gmail.com>
2020-09-28 16:58:47 +0200

    created new models of nutrifood proj

--- nutrifood/search/models.py
+++ nutrifood/search/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
-class Produit(models.Model):
+
+class Main_categories(models.Model):
+    category_name = models.CharField(max_length=20)
+
+
+class Products(models.Model):
+    code_id = models.CharField(max_length=200)
+    product_name = models.CharField(max_length=20)
     ingredient_text = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200)
+    category = models.ForeignKey(Main_categories, on_delete=models.CASCADE)
